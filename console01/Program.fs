30bfeda02920ea9d2b85581f0e93025d886f47e2
isaac <isaacbejaranopro@gmail.com>
2023-04-01 02:02:44 +0200

    console01

---
+++ console01/Program.fs
@@ -0,0 +1,14 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+// Learn more about F# at http://learn.microsoft.com/dotnet/fsharp
+
+open System
+
+// Define a function to construct a message to print
+let from whom =
+    sprintf "from %s" whom
+
+[<EntryPoint>]
+let main _argv =
+    let message = from "F#" // Call the function
+    printfn "Hello world %s" message
+    0 // return an integer exit code
